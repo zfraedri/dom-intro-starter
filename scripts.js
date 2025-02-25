abacf413776d779ebda5398eb02d02d3452b66cb
Zachary Fraedrich <zfraedri@asu.edu>
2025-02-25 14:42:00 -0700

    Updated JS
    Finished first 5 sections

--- scripts.js
+++ scripts.js
@@ -7,3 +7,27 @@ let images = document.getElementsByTagName("img");
 for(let image of images){
     image.width = "250";
 }
+
+// Third Section: Selecting Elements by Class Name
+let lightBlueSpans = document.getElementsByClassName("light-blue");
+let dkBlueSpans = document.getElementsByClassName("med-blue");
+
+for(let span of lightBlueSpans){
+    span.style.color = "#7C93A6"
+}
+for(let span of dkBlueSpans){
+    span.style.color = "#3C5E73"
+}
+
+// Fourth Section: Selecting Elements by CSS Selector (as a group)
+let svgs = document.querySelectorAll("#svgs svg");
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"]
+
+for(let i=0; i < svgs.length; i++){
+    svgs[i].style.stroke = colors[i];
+}
+
+// Fifth Section: Selecting Elements by CSS Selector (as the first element that matches on the page)
+document.querySelector("#query .bold").style.color = "#7C9EA6";
+
+// Sixth Section: Changing DOM Node Content
